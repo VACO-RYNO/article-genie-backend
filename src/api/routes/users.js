eb72588fdb9420e2c7b2efcb68d32e0242442cec
junbro <khm11904@gmail.com>
2022-06-09 16:17:17 +0900

    ✨ 아티클 생성 엔드포인트 구현

--- src/api/routes/users.js
+++ src/api/routes/users.js
@@ -1,12 +1,15 @@
 const { Router } = require("express");
 const joi = require("joi");
 joi.objectId = require("joi-objectid")(joi);
+const { startSession } = require("mongoose");
 
 const { catchAsync } = require("../../utils/asyncHandler");
 const verifyToken = require("../middlewares/verifyToken");
 const validate = require("../middlewares/validateSchema");
 
 const { User } = require("../../models/User");
+const { Article, joiArticleSchema } = require("../../models/Article");
+const Tag = require("../../models/Tag");
 
 const route = Router();
 
@@ -31,7 +34,46 @@ module.exports = app => {
   );
 
   route.get("/:user_id/articles", (req, res, next) => {});
-  route.post("/:user_id/articles", (req, res, next) => {});
+  route.post(
+    "/:user_id/articles",
+    validate(joi.object({ user_id: joi.objectId().required() }), "params"),
+    validate(joiArticleSchema, "body"),
+    catchAsync(async (req, res, next) => {
+      const { user_id } = req.params;
+      const { title, tag, lastVisitedSiteUrl } = req.body;
+      const session = await startSession();
+      let newArticle = {};
+
+      await session.withTransaction(async () => {
+        const { _id: tagId } = await Tag.findOne({ name: tag }, null, {
+          session,
+        }).lean();
+
+        const newArticleArr = await Article.create(
+          [
+            {
+              title,
+              tag: tagId,
+              lastVisitedSiteUrl,
+            },
+          ],
+          { session },
+        );
+        newArticle = newArticleArr[0].toObject();
+        delete newArticle.__v;
+
+        await User.updateOne(
+          { _id: user_id },
+          { $push: { myArticles: newArticle._id } },
+          { session },
+        );
+      });
+
+      session.endSession();
+
+      return res.status(201).json({ result: "ok", data: { ...newArticle } });
+    }),
+  );
   route.delete("/:user_id/articles/:article_id", (req, res, next) => {});
   route.put("/:user_id/articles/:article_id", (req, res, next) => {});
   route.patch("/:user_id/articles/:article_id", (req, res, next) => {});
